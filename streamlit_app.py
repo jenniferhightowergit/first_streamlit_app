ac4ba052580f5569074848bfa47a550f428a79f2
jenniferhightowergit <jennifer.susan.hightower@gmail.com>
2023-02-22 07:57:00 -0600

    changed header and added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🍲 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍳 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🥯 Avacado Toast')
 

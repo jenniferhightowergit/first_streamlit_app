e601f57d9a81718f242693c83f3e50413911c52e
jenniferhightowergit <jennifer.susan.hightower@gmail.com>
2023-02-22 07:58:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit
 
-streamlit.title('My Parents New HEalthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🍲 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
